f5118ed89b2943df52a535e2998c086554a6f441
Vivekanand P V <vivekanandpv@outlook.com>
2023-01-31 18:37:45 +0530

    done

--- Customer.Domain.Tests/CustomerControllerShould.cs
+++ Customer.Domain.Tests/CustomerControllerShould.cs
@@ -1,11 +1,50 @@
+using Moq;
+using NSubstitute;
+using Xunit.Abstractions;
+
 namespace Customer.Domain.Tests
 {
     public class CustomerControllerShould
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly ITestOutputHelper _helper;
 
+        public CustomerControllerShould(ITestOutputHelper helper)
+        {
+            _helper = helper;
+        }
+
+        [Fact]
+        public void GetMockCreatedInNSubstitute()
+        {
+            //  We first create the dependency
+            ICustomerService service = Substitute.For<ICustomerService>();
+
+            //  Dependency is now injected manually
+            CustomerController controller = new CustomerController(service);
+
+            //  An implementation is automatically created
+            //  Type is: Castle.Proxies.ObjectProxy
+            _helper.WriteLine(service.GetType().ToString());
+
+            Assert.NotNull(service);
+            Assert.IsAssignableFrom<ICustomerService>(service);
+        }
+
+        [Fact]
+        public void GetMockCreatedInMoq()
+        {
+            var mockWrapper = new Mock<ICustomerService>();
+            ICustomerService service = mockWrapper.Object;
+
+            //  Dependency is now injected manually
+            CustomerController controller = new CustomerController(service);
+
+            //  An implementation is automatically created
+            //  Type is: Castle.Proxies.ICustomerServiceProxy
+            _helper.WriteLine(service.GetType().ToString());
+
+            Assert.NotNull(service);
+            Assert.IsAssignableFrom<ICustomerService>(service);
         }
     }
 }
