820ebe785babaec849f5cccf2a8bcc0111d2f2b9
Vivekanand P V <vivekanandpv@outlook.com>
2023-01-31 19:03:07 +0530

    comments removed

--- Customer.Domain.Tests/CustomerControllerShould.cs
+++ Customer.Domain.Tests/CustomerControllerShould.cs
@@ -17,14 +17,13 @@ namespace Customer.Domain.Tests
         [Fact]
         public void ReturnCustomerForAnyIntIdNSubstitute()
         {
-            //  We first create the dependency
             ICustomerService service = Substitute.For<ICustomerService>();
 
-            //  Configuring the mock to return an object
+
             service.Get(Arg.Any<int>())
                 .Returns(new Customer { Email = "mock@gmail.com", Id = 0, Name = "Mr Mock" });
 
-            //  Dependency is now injected manually
+
             CustomerController controller = new CustomerController(service);
 
 
@@ -34,14 +33,13 @@ namespace Customer.Domain.Tests
         [Fact]
         public void ThrowArgumentExceptionForOddIdNSubstitute()
         {
-            //  We first create the dependency
             ICustomerService service = Substitute.For<ICustomerService>();
 
-            //  Custom configuration to throw exception
+
             service.Get(Arg.Is<int>(i => i % 2 != 0))
                 .Throws(new ArgumentException("Invalid id"));
 
-            //  Dependency is now injected manually
+
             CustomerController controller = new CustomerController(service);
 
             Assert.Throws<ArgumentException>(() => controller.Get(13));
@@ -51,13 +49,14 @@ namespace Customer.Domain.Tests
         public void ReturnCustomerForAnyIntIdMoq()
         {
             var mockWrapper = new Mock<ICustomerService>();
+
             mockWrapper
                 .Setup(s => s.Get(It.IsAny<int>()))
                 .Returns(new Customer { Email = "mock@gmail.com", Id = 0, Name = "Mr Mock" });
 
             ICustomerService service = mockWrapper.Object;
 
-            //  Dependency is now injected manually
+
             CustomerController controller = new CustomerController(service);
 
 
@@ -68,13 +67,14 @@ namespace Customer.Domain.Tests
         public void ThrowArgumentExceptionForOddIdMoq()
         {
             var mockWrapper = new Mock<ICustomerService>();
+
             mockWrapper
                 .Setup(s => s.Get(It.Is<int>(i => i % 2 != 0)))
                 .Throws(new ArgumentException("Invalid id"));
 
             ICustomerService service = mockWrapper.Object;
 
-            //  Dependency is now injected manually
+
             CustomerController controller = new CustomerController(service);
 
             Assert.Throws<ArgumentException>(() => controller.Get(13));
