646c503062862b4bfb2b685072c37f7e86aa1984
Vivekanand P V <vivekanandpv@outlook.com>
2023-01-31 18:23:32 +0530

    Add project files.

---
+++ Customer.Domain.Tests/CustomerControllerShould.cs
@@ -0,0 +1,11 @@
+namespace Customer.Domain.Tests
+{
+    public class CustomerControllerShould
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Customer.Domain/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿namespace Customer.Domain
+{
+    public class CustomerController
+    {
+        //  dependency
+        private readonly ICustomerService _customerService;
+
+        //  inverting the dependency
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        public Customer Get(int id)
+        {
+            return _customerService.Get(id);    //  delegation
+        }
+
+        public void Save(Customer customer)
+        {
+            _customerService.Save(customer);
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public interface ICustomerService
+    {
+        void Save(Customer customer);
+        Customer Get(int id);
+    }
+}
