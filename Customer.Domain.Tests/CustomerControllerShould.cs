fb76dfbfe1260c78f77f7f0bbb46ac6cbb9384ec
Vivekanand P V <vivekanandpv@outlook.com>
2023-01-31 19:29:51 +0530

    done

--- Customer.Domain.Tests/CustomerControllerShould.cs
+++ Customer.Domain.Tests/CustomerControllerShould.cs
@@ -21,16 +21,18 @@ namespace Customer.Domain.Tests
 
             service.Get(Arg.Any<int>())
                 .Returns(
-                    new Customer { Email = "mock@gmail.com", Id = 1, Name = "Mr Mock" }, 
-                    new Customer { Email = "mock2@gmail.com", Id = 2, Name = "Mr Mock2" }, 
-                    new Customer { Email = "mock3@gmail.com", Id = 3, Name = "Mr Mock3" }
-                    );
+                    _ => new Customer { Email = "mock@gmail.com", Id = 1, Name = "Mr Mock" }, 
+                    _ => new Customer { Email = "mock2@gmail.com", Id = 2, Name = "Mr Mock2" }, 
+                    _ => new Customer { Email = "mock3@gmail.com", Id = 3, Name = "Mr Mock3" },
+                    _ => throw new ArgumentException("Invalid id")
+                    );   //  throws at 4th invocation
 
             CustomerController controller = new CustomerController(service);
 
             Assert.Equal(1, controller.Get(12).Id);
             Assert.Equal(2, controller.Get(12).Id);
             Assert.Equal(3, controller.Get(12).Id);
+            Assert.Throws<ArgumentException>(() => controller.Get(12));
         }
 
         [Fact]
